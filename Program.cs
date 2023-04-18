1762a93ad5d2b500f5ac7c30f3b36b3f928cfe0d
CodeGoblin <jmar4593@live.com>
2023-04-18 10:02:26 -0400

    It was giving me errors cs1519 for every line, but now its working?

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Classes;
+
+var account = new BankAccount("<name>", 1000);
+Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
